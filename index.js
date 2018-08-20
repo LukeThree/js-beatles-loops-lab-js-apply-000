72904835f7d43002dcd290d4efbaa1536ae39e0b
Luke Matthews <onelukeshow@googlemail.com>
2018-08-20 20:17:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  
+  return array;
+  
+  
+}
+
+function johnLennonFacts(){
+  
+  
+}
+
+function iLoveTheBeatles(){
+  
+  
+}
