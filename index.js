b865ac5a12be17d71943e385acf9fb8919f0a18f
Luke Matthews <onelukeshow@googlemail.com>
2018-08-20 20:23:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians,instruments) {
 function johnLennonFacts(facts) {
   var i = 0
   while (i < facts.length) {
-    array[i] += '!!!';
+    facts[i] += '!!!';
     i++;
   }
   return facts;
